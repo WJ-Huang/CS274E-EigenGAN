b6a5055059d22fa75d4bf0568a6542a74ce7f287
calvin4770 <calvin4770@gmail.com>
2022-11-28 08:10:17 -0800

    Fix generator hinge loss

--- loss.py
+++ loss.py
@@ -5,7 +5,7 @@ def discriminator_hinge_loss(real_pred, fake_pred):
     return F.relu(1 - real_pred).mean() + F.relu(1 + fake_pred).mean()
 
 def generator_hinge_loss(fake_pred):
-    return F.relu(1 - fake_pred).mean()
+    return -fake_pred.mean()
 
 def r1_loss(output, input):
     grad = torch.autograd.grad(outputs=output.sum(), inputs=input, create_graph=True)[0]
