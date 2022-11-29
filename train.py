31844cf30ab319345dd7a5c6f59d36854b3e1052
calvin4770 <calvin4770@gmail.com>
2022-11-28 22:35:52 -0800

    Added EMA and augmentation

--- train.py
+++ train.py
@@ -1,11 +1,21 @@
 import torch
 import model
 import loss
+import copy
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from dataset import Dataset, infinite_loader
 from sample import gen_and_save_sample
+from augmentation import DiffAugment
+from functools import partial
 from config import *
+
+def accumulate(model1, model2, decay=0.999):
+    par1 = dict(model1.named_parameters())
+    par2 = dict(model2.named_parameters())
+
+    for k in par1.keys():
+        par1[k].data.mul_(decay).add_(par2[k].data, alpha=1 - decay)
 
 if __name__ == "__main__":
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -18,6 +28,7 @@ if __name__ == "__main__":
         base_channels=BASE_CHANNELS,
         max_channels=MAX_CHANNELS
     ).to(device)
+    g_ema = copy.deepcopy(generator).eval()
 
     discriminator = model.Discriminator(
         size=SIZE, 
@@ -59,6 +70,9 @@ if __name__ == "__main__":
         )
     )
 
+    augment = DiffAugment(policy='color,translation,cutout', p=0.6)
+    ema = partial(accumulate, decay=0.5 ** (BATCH / (10 * 1000)))
+
     for step in range(STEPS):
         real = next(loader).to(device)
         with torch.no_grad():
@@ -89,15 +103,17 @@ if __name__ == "__main__":
         generator_loss.backward()
         g_optim.step()
 
+        ema(g_ema, generator)
+
         print(f'Step: {step}/{STEPS}, D_loss: {discriminator_loss.item()}, G_loss: {generator_loss.item()}')
         training_loss.write(f"{discriminator_loss.item()}, {generator_loss.item()}\n")
 
         if step > 0 and step % SAVE_EVERY == 0:
             torch.save(discriminator.state_dict(), f"model_checkpoints/discriminator_step_{step}.ckpt")
             torch.save(generator.state_dict(), f"model_checkpoints/generator_step_{step}.ckpt")
+            torch.save(g_ema.state_dict(), f"model_checkpoints/g_ema_step_{step}.ckpt")
         
         if step > 0 and step % SAMPLE_EVERY == 0:
-            gen_and_save_sample(generator, 16, 4, f"samples/samples_{step}.jpg")
+            gen_and_save_sample(g_ema, 16, 4, f"samples/samples_{step}.jpg")
 
-            
     training_loss.close()
