8c0e39641a7197dd828c012fb5c2f227cb4e2f27
calvin4770 <calvin4770@gmail.com>
2022-11-29 17:44:23 -0800

    Removed ema and augmentation

--- train.py
+++ train.py
@@ -6,16 +6,8 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 from dataset import Dataset, infinite_loader
 from sample import gen_and_save_sample
-from augmentation import augment
-from functools import partial
 from config import *
 
-def accumulate(model1, model2, decay=0.999):
-    par1 = dict(model1.named_parameters())
-    par2 = dict(model2.named_parameters())
-
-    for k in par1.keys():
-        par1[k].data.mul_(decay).add_(par2[k].data, alpha=1 - decay)
 
 if __name__ == "__main__":
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -29,7 +21,6 @@ if __name__ == "__main__":
         max_channels=MAX_CHANNELS
     ).to(device)
     
-    g_ema = copy.deepcopy(generator).eval()
 
     discriminator = model.Discriminator(
         size=SIZE, 
@@ -77,16 +68,14 @@ if __name__ == "__main__":
         )
     )
 
-    ema = partial(accumulate, decay=0.5 ** (BATCH / (10 * 1000)))
-
     for step in range(START_FROM+1, STEPS):
         real = next(loader).to(device)
         
         with torch.no_grad():
             fake = generator.sample(BATCH)
 
-        real_pred = discriminator(augment(real, 0.6))
-        fake_pred = discriminator(augment(fake, 0.6))
+        real_pred = discriminator(real)
+        fake_pred = discriminator(fake)
 
         # adverserial loss
         discriminator_loss = loss.discriminator_hinge_loss(real_pred, fake_pred)
@@ -97,7 +86,7 @@ if __name__ == "__main__":
         # R1 penalty, lazy penalty
         if step % REG_EVERY == 0:
             real.requires_grad = True
-            real_pred = discriminator(augment(real, 0.6))
+            real_pred = discriminator(real)
             r1 = loss.r1_loss(real_pred, real) * R1_PENALTY_COEFFICIENT
             discriminator.zero_grad()
             r1.backward()
@@ -110,17 +99,14 @@ if __name__ == "__main__":
         generator_loss.backward()
         g_optim.step()
 
-        ema(g_ema, generator)
-
         print(f'Step: {step}/{STEPS}, D_loss: {discriminator_loss.item()}, G_loss: {generator_loss.item()}')
         training_loss.write(f"{discriminator_loss.item()}, {generator_loss.item()}\n")
 
         if step > 0 and step % SAVE_EVERY == 0:
             torch.save(discriminator.state_dict(), f"model_checkpoints/discriminator_step_{step}.ckpt")
             torch.save(generator.state_dict(), f"model_checkpoints/generator_step_{step}.ckpt")
-            torch.save(g_ema.state_dict(), f"model_checkpoints/g_ema_step_{step}.ckpt")
         
         if step > 0 and step % SAMPLE_EVERY == 0:
-            gen_and_save_sample(g_ema, 16, 4, f"samples/samples_{step}.jpg")
+            gen_and_save_sample(generator, 16, 4, f"samples/samples_{step}.jpg")
 
     training_loss.close()
