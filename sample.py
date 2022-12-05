c413b3a52f515bda4c20a111e98daa609bb94729
calvin4770 <calvin4770@gmail.com>
2022-12-04 18:31:16 -0800

    Added normalization of U

--- sample.py
+++ sample.py
@@ -2,10 +2,18 @@ import torch
 import torchvision
 import model
 import numpy as np
+import copy
 
 from config import *
 
 TRAVERSE = True
+
+def deg_ortho(U, device):
+    with torch.no_grad():
+        M = copy.deepcopy(U)
+        norms = torch.norm(U, dim=1)
+        M = (M.T / norms).T
+    return torch.sum((M.matmul(M.T) - torch.eye(M.shape[0], device=device))**2)
 
 def gen_and_save_sample(generator, n_samples, nrow, path):
     with torch.no_grad():
@@ -19,19 +27,19 @@ def traverse_and_save_samples(generator, num_samples, range_len, samples_per_ran
     js = np.linspace(-range_len, range_len, samples_per_range)
     for layer in range(n_layers):
         U = generator.blocks[layer].subspacelayer.U
-        print(f"||UUT-I||_F^2/n^2={torch.mean((U.matmul(U.T) - torch.eye(U.shape[0], device=generator.getDevice()))**2)}")
+        print(f"||MMT-I||_F^2={deg_ortho(U, generator.getDevice())*1000}")
         for dim in range(n_dim):
             print(f"Layer {layer}, dimension {dim}, Lij={generator.blocks[layer].subspacelayer.L[dim]}")
             imgs = []
             for j in js:
-                zs = z_samples.clone()
+                zs = copy.deepcopy(z_samples)
                 zs[:, layer, dim] = j
                 with torch.no_grad():
                     img = generator((epsilon_samples, zs)).cpu()
-                    img = torch.cat([_img for _img in img], dim=1)
-                imgs.append(img)
-            imgs = torch.cat(imgs, dim=2)
-            torchvision.utils.save_image(imgs, f"samples/traverse_L{layer}_D{dim}.jpg", normalize=True,value_range=(-1, 1))
+                    imgs.append(img)
+            imgs = torch.stack(imgs, dim=0).swapaxes(0, 1)
+            imgs = imgs.reshape(num_samples * samples_per_range, 3, SIZE, SIZE)
+            torchvision.utils.save_image(imgs, f"samples/traverse_L{layer}_D{dim}.jpg", normalize=True, value_range=(-1, 1), nrow=samples_per_range)
 
 if __name__ == "__main__":
 
